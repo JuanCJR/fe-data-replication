a77baafe305b6dcb69f91d3811eb0df54f6b583d
Juan C Jimenez <jcjimenezr@falabella.cl>
2023-04-10 15:32:27 -0400

    first commit

---
+++ src/pages/data-groups/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function DataGroupsPage() {
+  return <div>DataGroupsPage</div>
+}
---
+++ src/pages/process/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function ProcessPage() {
+  return <div>ProcessPage</div>
+}
